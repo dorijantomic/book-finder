6a37d8466f1adf1f17cd847564ae1d0c59a7968d
[Dorijan] <[dorijan11@gmail.com]>
2019-05-30 18:56:50 +0200

    Implemented Navbar, and Book Card

---
+++ src/containers/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component,Fragment } from 'react'
+import Book from '../../components/Book/Book'
+class Form extends Component {
+    render() {
+        return (
+           <Fragment>
+               <Book />
+           </Fragment>
+        )
+    }
+}
+
+export default Form
