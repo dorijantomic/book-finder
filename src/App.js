c8bfb833117b2a21fcaa93748e5e66a04d8bcf10
[Dorijan] <[dorijan11@gmail.com]>
2019-05-31 19:53:04 +0200

    implemented search and some layout features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Form from './containers/Form/Form'
 import Nav from './components/UI/Nav/Nav'
+import { createMuiTheme, withStyles,  } from '@material-ui/core/styles';
+import {ThemeProvider  } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiPaper: {
+      root: {
+          margin: '0 auto'      
+      }
+    }
+  }
+})
+
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
     <React.Fragment>
       <Nav />
       <Form />
     </React.Fragment>
+    </ThemeProvider>
   )
 }
 
--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,13 +1,64 @@
-import React, { Component,Fragment } from 'react'
-import Book from '../../components/Book/Book'
+import React, { Component, Fragment } from "react";
+import Book from "../../components/Book/Book";
+import { TextField, Button ,Grid} from "@material-ui/core";
+
 class Form extends Component {
-    render() {
-        return (
-           <Fragment>
-               <Book />
-           </Fragment>
-        )
-    }
+  state = {
+    books: null,
+    search: null
+  };
+
+  handleChange = (e) =>{
+      this.setState({
+          search: e.target.value
+      })
+  }
+
+  handleClick = (e) => {
+    debugger;
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.search}`)
+      .then(res => {
+          if(res.status === 200) {
+              return res.json()
+          } else {
+              console.log('error happened')
+          }
+      }).then(res => this.setState({books: res})).then(res => console.log(this.state.books))
+  }
+
+  render() {
+    return (
+      <Grid container justify='center' direction='row'>
+         <Fragment>
+          <form onSubmit={(e) => e.preventDefault()}>
+          <TextField
+          onChange = {this.handleChange}
+          id="outlined-full-width"
+          label="Label"
+          style={{ margin: 8 }}
+          placeholder="Placeholder"
+          helperText="Full width!"
+         
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <Button variant='outlined' onClick={this.handleClick}>Search</Button>
+          </form>
+        <Grid item xs={12}>
+          <Grid container justify='center' >
+          {this.state.books === null ? null: <Book bookData={this.state.books} /> }
+          </Grid>
+        
+        </Grid>
+        
+      </Fragment>
+      </Grid>
+     
+    );
+  }
 }
 
-export default Form
+export default Form;
